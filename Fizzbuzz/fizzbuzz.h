f830f8b46022d356a563b27b542e3f8d340261a4
Eric Farias Borges <ericfborges@gmail.com>
2020-05-14 22:29:28 -0400

    Moved fizzbuzz function to header + cpp file; added threading

---
+++ Fizzbuzz/fizzbuzz.h
@@ -0,0 +1,20 @@
+//
+//  fizzbuzz.hpp
+//  Fizzbuzz
+//
+//  Created by Eric Borges on 5/14/2020.
+//  Copyright © 2020 Eric Borges. All rights reserved.
+//
+
+#ifndef fizzbuzz_hpp
+#define fizzbuzz_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void fizzbuzz(int end, int fizz, int buzz);
+
+#endif /* fizzbuzz_hpp */
--- Fizzbuzz/main.cpp
+++ Fizzbuzz/main.cpp
@@ -7,25 +7,61 @@
 //
 
 #include <iostream>
+#include <thread>
+#include "fizzbuzz.h"
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::thread;
+
+
+
 
 int main() {
-    // insert code here...
+    char Mode = 's', Continue = 'y';
+    int max = 1, fizz = 3, buzz = 5;
     
-    int fizz = 3, buzz = 7;
-    for (int i = 1; i <= 100; i++)
+    cout << "Welcome to the Fizzbuzz program!";
+    while (Continue == 'y' || Continue == 'Y')
     {
-        if (i%fizz == 0)    //if remainder of i/fizz is equal to zero
-        cout << "fizz";
+        cout << "\nWhat mode would you like to use?\n";
         
-        if (i%buzz == 0)    //if remainder of i/buzz is equal to zero
+        cout << "\n(S)tandard: Fizz = 3, Buzz = 5\n";
+        cout << "\nOr set (C)ustom Fizz/Buzz values\n";
+        cin >> Mode;
+        
+        cout << "\nHow far do you want Fizzbuzz to go? ";
+        cin >> max;
+        
+        if (Mode == 's' || Mode == 'S')
+            {
+                fizz = 3; buzz = 5;
+                thread alpha(fizzbuzz, max, 3, 5);
+                alpha.join();
+            }
+        else if (Mode == 'c' || Mode == 'C')
         {
-        cout << "buzz";
+            cout << "What values do you want for Fizzbuzz?\n";
+            cout << "Fizz: ";
+            cin >> fizz;
+            cout << "Buzz: ";
+            cin >> buzz;
+            //fizzbuzz(max, fizz, buzz);
+            thread bravo(fizzbuzz, max, fizz, buzz);
+            bravo.join();
         }
-        if (i%fizz != 0 && i%buzz != 0) cout << i;  //if remainder of i/fizz and i/buzz is not zero
-        cout << endl;   //new line
-    }
+        else cout << "Wrong input!";
         
+        cout << "\nGo again? \n";
+        cin >> Continue;
+            
+    }
+    
+    
+    
+    
+    
     return 0;
 }
+
